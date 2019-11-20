7617205abc698e4c9019b44917e940b2e3451be1
ZC <zhoucan1999@outlook.com>
2019-11-20 09:34:48 +0000

    Query more data

--- Get_data_from_GitHub/app.js
+++ Get_data_from_GitHub/app.js
@@ -4,10 +4,10 @@ const fetch = require('node-fetch');
 const accessToken = 'accessToken_has_been_deleted';
 
 // This GraphQL query is to get the D3 repo information.
-const query = `
+var query = `
 query {
   D3_whole_project:repositoryOwner(login:"d3"){
-    D3_repo: repositories(first:5) {
+    D3_repo: repositories(first:10) {
       D3_sub_repo_info:edges{
         each_repo:node{
           name
@@ -42,7 +42,7 @@ fetch('https://api.github.com/graphql', {
 
         // Output a JSON file named D3_repo.json to store the D3 repo information.
         var fs = require('fs');
-        var outputFilename = './D3_repo.json';
+        var outputFilename = './D3_data_0.json';
         fs.writeFile(outputFilename, body, function (err) {
             if (err) {
                 console.log(err);
@@ -54,3 +54,46 @@ fetch('https://api.github.com/graphql', {
     )
     .catch(error => console.error(error));
   
+/* Second */
+query = `
+query {
+  D3_whole_project:repositoryOwner(login:"d3"){
+    D3_repo: repositories(first: 11, orderBy: {field: STARGAZERS, direction: DESC}) {
+      D3_sub_repo_info:edges{
+        each_repo:node{
+         name
+         forkCount
+         diskUsage
+         stargazers{
+          totalCount
+        }
+        }
+      }
+    }
+  }
+}`;
+
+  
+// Using fetch to access GitHub GraphQL API.
+fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    body: JSON.stringify({ query }),
+    headers: {
+        'Authorization': `Bearer ${accessToken}`,
+    },
+}).then(res => res.text())
+    .then(body => {
+
+        // Output a JSON file named D3_repo.json to store the D3 repo information.
+        var fs = require('fs');
+        var outputFilename = './D3_data_1.json';
+        fs.writeFile(outputFilename, body, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("JSON saved to " + outputFilename);
+            }
+        });
+    }
+    )
+    .catch(error => console.error(error));
