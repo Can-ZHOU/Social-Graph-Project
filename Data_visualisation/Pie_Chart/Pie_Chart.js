3065efd6ecfe89089dfdd3180adff2f3638833f5
ZC <zhoucan1999@outlook.com>
2019-11-24 21:48:17 +0000

    Fisrt version of pie chart

---
+++ Data_visualisation/Pie_Chart/Pie_Chart.js
@@ -0,0 +1,13 @@
+
+var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+        json: json_data_forkCount,
+        type : 'pie',
+        onclick: function (d, i) { console.log("onclick", d, i); },
+        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+    }
+});
+
+
---
+++ Data_visualisation/Pie_Chart/Pie_Chart_JSON.js
@@ -0,0 +1,5 @@
+var json_data_forkCount = {"d3-shape": 227, "d3-plugins": 709, "d3-queue": 148, "d3-scale": 232, "d3-geo-projection": 167};
+
+var json_data_diskUsage = {"d3-shape": 1848, "d3-plugins": 676, "d3-queue": 119, "d3-scale": 752, "d3-geo-projection": 20143};
+
+var json_data_star = {"d3-shape": 1894, "d3-plugins": 1809, "d3-queue": 1412, "d3-scale": 1153, "d3-geo-projection": 782};
