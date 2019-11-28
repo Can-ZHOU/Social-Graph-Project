f17fc9223928369721997206296089dcd986e9b8
ZC <zhoucan1999@outlook.com>
2019-11-28 04:03:19 +0000

    Gain new data

--- Get_data_from_GitHub/app.js
+++ Get_data_from_GitHub/app.js
@@ -54,6 +54,7 @@ fetch('https://api.github.com/graphql', {
     )
     .catch(error => console.error(error));
   
+
 /* Second */
 query = `
 query {
@@ -87,6 +88,71 @@ fetch('https://api.github.com/graphql', {
         // Output a JSON file named D3_repo.json to store the D3 repo information.
         var fs = require('fs');
         var outputFilename = './D3_data_1.json';
+        fs.writeFile(outputFilename, body, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("JSON saved to " + outputFilename);
+            }
+        });
+    }
+    )
+    .catch(error => console.error(error));
+
+    
+/* Third */
+query = `
+query {
+  D3_whole_project:repositoryOwner(login:"d3"){
+    D3_repo: repositories(first: 6, orderBy: {field: STARGAZERS, direction: DESC}) {
+      D3_sub_repo_info:edges{
+        each_repo:node{
+          name
+          forks(last:3){
+            fork_e:edges{
+              fork_n:node{
+                name:owner{
+                  login
+                }
+              }
+            }
+          }
+          issues(last:3){
+            issue_e:edges{
+              issue_n:node{
+                name:author{
+                  login
+                }
+              }
+            }
+          }
+          stargazers(last:3){
+            start_e:edges{
+              star_n:node{
+                login
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`;
+
+  
+// Using fetch to access GitHub GraphQL API.
+fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    body: JSON.stringify({ query }),
+    headers: {
+        'Authorization': `Bearer ${accessToken}`,
+    },
+}).then(res => res.text())
+    .then(body => {
+
+        // Output a JSON file named D3_repo.json to store the D3 repo information.
+        var fs = require('fs');
+        var outputFilename = './D3_data_2.json';
         fs.writeFile(outputFilename, body, function (err) {
             if (err) {
                 console.log(err);
